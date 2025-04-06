db14cd92a2e0059eec913df0264569a2cdf7299d
santhoshmk <connectwithsanthoshmk@gmail.com>
2025-04-07 02:28:54 +0530

    done template adaption with js

---
+++ assets/js/reg.js
@@ -0,0 +1,31 @@
+const reg=document.getElementById("reg");
+function toggleReg(){
+    reg.classList.toggle("visible")
+    reg.parentElement.classList.toggle("blurblackbg");
+    document.documentElement.classList.toggle("overflow-hidden")
+    if (reg.classList.contains('visible')) {
+        history.pushState({},"","?internReg=true")
+    } else {
+        history.pushState({},"","/")
+    }
+
+}
+
+
+function getEvent(){
+    const params = new URLSearchParams(window.location.search);
+      const value = params.get('internReg');
+      console.log(value);
+      const isCurrentlyVisible = reg.classList.contains('visible');
+       if (value === "true" && !isCurrentlyVisible) {
+        reg.classList.add("visible");
+        reg.parentElement.classList.add("blurblackbg");
+        document.documentElement.classList.add("overflow-hidden");
+    } else if (value !== "true" && isCurrentlyVisible) {
+        reg.classList.remove("visible");
+        reg.parentElement.classList.remove("blurblackbg");
+        document.documentElement.classList.remove("overflow-hidden");
+    }
+  }
+  window.addEventListener('load', getEvent)
+  window.addEventListener("popstate",getEvent)
