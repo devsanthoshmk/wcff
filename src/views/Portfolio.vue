6f286506da14d80a721568ad33f98e8f501d5944
santhoshmk <connectwithsanthoshmk@gmail.com>
2025-04-13 15:31:20 +0530

    fixed projects:nth-of-type(2n) right left

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -23,6 +23,7 @@
           </h5>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore officiis qui, non recusandae corrupti corporis velit repellat facilis quos quam nisi, itaque aperiam nostrum quae doloremque facere fugit quo! Sit dolore earum assumenda fugiat tempora distinctio nihil iusto rem fuga incidunt quam necessitatibus doloremque molestias vitae, commodi ipsum nemo expedita!</p>
         </div>
+        <Tag icon="pi pi-calendar"  value="2020" severity="contrast" class="relative left-1/2 -translate-x-1/2 mt-4 text-lg px-4 py-2 h-10 [&_i.pi]:text-2xl" />
         <div class="projects clear-both m-4 mr-12 p-4 relative w-[calc(50%-20px)]">
           <h5>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, dolore.
@@ -37,9 +38,7 @@
 
 <style>
   .projects:nth-of-type(2n) {
-        float: right;
-        margin: 1rem;
-        border-color: tan;
+      right: -50%;
     }
 </style>
 
